91ffe481b7289cd06cfa046a793f15d628a26739
shrawanamit <amitkumar06111@gmail.com>
2020-05-11 15:06:55 +0530

    Abality for the  analyser to load the indian state informattion from the csv file if incorrect throw custom exception

---
+++ src/main/java/com/censusanalyser/CensusAnalyserException.java
@@ -0,0 +1,19 @@
+package com.censusanalyser;
+
+public class CensusAnalyserException extends Throwable {
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
